fbb157ca828c0a4ea4820513f963067ca1a14f95
Rails Abam <abamsunday5@gmail.com>
2022-05-31 00:30:35 +0100

    commit

---
+++ str.rb
@@ -0,0 +1,20 @@
+ name = "Rails"
+ save = name.downcase() #<= upcase, strip, length,include,index
+ puts save
+
+
+ #< Number
+ num = 20.5643
+ number = num.round() #<= abs,ceils, floor, math
+ puts number # Math.sqrt,log,sin.
+
+
+print "Enter Your name: "
+name = gets.chomp()
+puts "Enter Your Age: "
+age = gets.chomp()
+puts ("Hello my name is " + name + " and I'm " + age + " years old")
+
+#Arrary
+Frinds = Array["Rails", "tim", "kingsly", "Ruby", 34, 65, 12, 4]
+puts Frinds
